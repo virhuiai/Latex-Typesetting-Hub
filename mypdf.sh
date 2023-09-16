6a611340704c9e299868247a7e5e09b3dd4a91c9
virhuiai <virhuiai@virhuiai.com>
2023-09-16 17:31:46 +0800

    。

--- mypdf.sh
+++ mypdf.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 function myxelatex() {
     if [[ ! -f $1 ]]; then
-      echo "${1}不是文件"
-      return 1
+        echo "${1}不是文件"
+        return 1
     fi
 
     # 使用 python 获取文件的完整路径
@@ -11,16 +11,20 @@ function myxelatex() {
     # 获取文件名
     filename=$(basename $1)
 
+    # 如果没有传入第二个参数，那么默认目录为 /Volumes/RamDisk/
+    output_dir=${2:-/Volumes/RamDisk/}
+
     # 建立 xelatexOut 目录
-    mkdir -p /Volumes/RamDisk/xelatexOut
+    mkdir -p "${output_dir}xelatexOut"
 
     # 写入文件
-    output_file="/Volumes/RamDisk/xelatexOut/$filename"
+    output_file="${output_dir}xelatexOut/$filename"
     if [[ ! -f $output_file ]]; then
         echo "\\input{$full_path}" > $output_file
     fi
 
     # 调用 xelatex
-    xelatex --output-directory=/Volumes/RamDisk/xelatexOut/ -synctex=1 -shell-escape $output_file
+    xelatex --output-directory="${output_dir}xelatexOut/" -synctex=1 -shell-escape "${@:3}" $output_file 
 }
-myxelatex $1
+myxelatex "$@"
+# ln -s /Users/virhuiai/hlProjects/Latex-Typesetting-Hub/mypdf.sh /usr/local/bin/mypdf
